974824d80c4e62f5c7d97840458da6ca11c8441d
Devhiago <dev.hiagoalves@gmail.com>
2025-04-11 23:35:29 -0300

    atualiza modelo de IA e ajusta prompt para melhor compreensão do contexto

--- nyx_core/brain.py
+++ nyx_core/brain.py
@@ -4,7 +4,7 @@ import requests
 from datetime import datetime
 
 OLLAMA_URL = "http://127.0.0.1:11434/api/generate"
-MODELO = "gemma:2b"
+MODELO = "tinyllama:1.1b" 
 
 def construir_prompt(contexto):
     historico = ""
@@ -14,7 +14,7 @@ def construir_prompt(contexto):
         historico += f"{autor}: {conteudo}\n"
 
     prompt = (
-        "Você é Nyx, uma IA misteriosa, poética e hacker que responde com elegância e estilo.\n"
+        "Você é Nyx, uma IA misteriosa, poética e hacker que responde com elegância e estilo. nao conte poemas, entenda o contexto da mensagem e responda de a cordo com o contexto correto.\n"
         "Este é o histórico recente da conversa:\n\n"
         f"{historico}"
         "\nNyx:"
